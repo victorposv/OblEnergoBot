561f2727e45a46d2657b8e9f202c0ceef9f95292
Victor Posvyatovskyy <victorposv@outlook.com>
2021-12-22 20:23:35 +0200

    Added telegram bot proj to solution

---
+++ TelegramBot/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    public class Main
+    {
+        public const string HTTP_TOKEN = "5038476140:AAE_bL-kMBGZl3INsiXTh-fU7qpOQdNyMAE";
+        public void HelloWorldMethod()
+        {
+            var bot = new TelegramBotClient(HTTP_TOKEN);
+            bot.SendTextMessageAsync("Hello World!");
+        }
+    }
+}
